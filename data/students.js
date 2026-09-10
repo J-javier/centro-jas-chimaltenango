b21c3ba3a8d1d80e1cc32c8621c74689afb91fbf
J-javier <jose360pelico@gmail.com>
2026-09-09 21:15:59 -0600

    Update lesson numbers for students in SEED_STUDENTS for accuracy semana 17 julio completada

--- data/students.js
+++ data/students.js
@@ -11,29 +11,42 @@ const SEED_STUDENTS = {
   // 19 de junio — lecciones 1 y 2
   "Gabriela Orellana": [1, 2],
   "Julia Gomez": [1, 2, 3, 4],
-  "Luis Coyoy": [1, 2, 3, 4, 5, 6],
+  "Luis Coyoy": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
   "Mia Orellana": [1, 2, 3, 4],
-  "Yasmin Lorenzo": [1, 2, 3, 4, 5, 6],
+  "Yasmin Lorenzo": [1, 2, 3, 4, 5, 6, 7, 8],
 
   // 26 de junio — lecciones 3 y 4
   "Camila Orellana": [3, 4],
-  "Alisson Lorenzo": [3, 4, 5, 6],
-  "Saida Monroy": [3, 4, 5, 6],
+  "Alisson Lorenzo": [3, 4, 5, 6, 7, 8],
+  "Saida Monroy": [3, 4, 5, 6, 7, 8],
   "Elder Osorio": [3, 4],
-  "Hassen Pinto": [3, 4, 5, 6],
+  "Hassen Pinto": [3, 4, 5, 6, 7, 8],
   "Emanuel Ramirez": [3, 4],
   "Axel Lorenzo": [3, 4],
-  "Fredy Ramirez": [3, 4, 5, 6],
+  "Fredy Ramirez": [3, 4, 5, 6, 7, 8],
   "Rafael Choy": [3, 4],
   "Dayli Rojcho": [3, 4],
-  "Telma Choc": [3, 4],
-  "Kevin Choc": [3, 4],
-  "Jose Ramirez": [3, 4, 5, 6],
-  "Miriam Coyoy": [3, 4, 5, 6],
+  "Telma Choc": [3, 4, 7, 8],
+  "Kevin Choc": [3, 4, 7, 8],
+  "Jose Ramirez": [3, 4, 5, 6, 7, 8],
+  "Miriam Coyoy": [3, 4, 5, 6, 7, 8, 9, 10],
 
   // 3 de julio — lecciones 5 y 6
-  "Melany Jimenez": [5, 6], 
-  "Alexander Boc": [5, 6],
-  "Nefertiti Cua": [5, 6],
+  "Melany Jimenez": [5, 6],
+  "Alexander Boc": [5, 6, 7, 8],
+  "Nefertiti Cua": [5, 6, 7, 8],
   "Harry Cua": [5, 6],
+
+  // 10 de julio — lecciones 7 y 8
+  "Alejandro Sanchez": [7, 8],
+  "Hector Mejia": [7, 8], // apellido poco legible ("Mejin"/"Mejía"), confirmar
+  "Eva Guaran": [7, 8], // nombre poco legible, confirmar
+  "Jared Cua": [7, 8],
+  "Ludwin Cuca": [7, 8],
+  "Samuel Alvarez": [7, 8],
+  "William Israel": [7, 8],
+  "Yuliet Riz": [7, 8],
+  "Benjamin Chojcholaj": [7, 8],
+  "Carlos Reyes": [7, 8],
+  "Leonardo Pineda": [7, 8],
 };
