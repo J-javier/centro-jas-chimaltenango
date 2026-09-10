8ab6e6df4a43f230bf448a96911d62f3b0c7274e
J-javier <jose360pelico@gmail.com>
2026-09-09 21:22:31 -0600

    Update lesson numbers for students in SEED_STUDENTS to include lessons 11 and 12; add new entries for Carlos Pelico

--- data/students.js
+++ data/students.js
@@ -11,38 +11,38 @@ const SEED_STUDENTS = {
   // 19 de junio — lecciones 1 y 2
   "Gabriela Orellana": [1, 2, 9, 10],
   "Julia Gomez": [1, 2, 3, 4],
-  "Luis Coyoy": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  "Luis Coyoy": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
   "Mia Orellana": [1, 2, 3, 4],
-  "Yasmin Lorenzo": [1, 2, 3, 4, 5, 6, 7, 8],
+  "Yasmin Lorenzo": [1, 2, 3, 4, 5, 6, 7, 8, 11, 12],
 
   // 26 de junio — lecciones 3 y 4
   "Camila Orellana": [3, 4],
-  "Alisson Lorenzo": [3, 4, 5, 6, 7, 8],
-  "Saida Monroy": [3, 4, 5, 6, 7, 8],
+  "Alisson Lorenzo": [3, 4, 5, 6, 7, 8, 11, 12],
+  "Saida Monroy": [3, 4, 5, 6, 7, 8, 11, 12],
   "Elder Osorio": [3, 4],
-  "Hassen Pinto": [3, 4, 5, 6, 7, 8],
+  "Hassen Pinto": [3, 4, 5, 6, 7, 8, 11, 12],
   "Emanuel Ramirez": [3, 4],
   "Axel Lorenzo": [3, 4],
-  "Fredy Ramirez": [3, 4, 5, 6, 7, 8],
+  "Fredy Ramirez": [3, 4, 5, 6, 7, 8, 11, 12],
   "Rafael Choy": [3, 4],
   "Dayli Rojcho": [3, 4],
   "Telma Choc": [3, 4, 7, 8],
   "Kevin Choc": [3, 4, 7, 8],
-  "Jose Ramirez": [3, 4, 5, 6, 7, 8],
-  "Miriam Coyoy": [3, 4, 5, 6, 7, 8, 9, 10],
+  "Jose Ramirez": [3, 4, 5, 6, 7, 8, 11, 12],
+  "Miriam Coyoy": [3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
 
   // 3 de julio — lecciones 5 y 6
   "Melany Jimenez": [5, 6],
-  "Alexander Boc": [5, 6, 7, 8, 9, 10],
+  "Alexander Boc": [5, 6, 7, 8, 9, 10, 11, 12],
   "Nefertiti Cua": [5, 6, 7, 8],
   "Harry Cua": [5, 6],
 
   // 10 de julio — lecciones 7 y 8
   "Alejandro Sanchez": [7, 8],
   "Hector Mejia": [7, 8], // apellido poco legible ("Mejin"/"Mejía"), confirmar
-  "Eva Guaran": [7, 8], // nombre poco legible, confirmar
+  "Eva Guaran": [7, 8, 11, 12],
   "Jared Cua": [7, 8],
-  "Ludwin Cuca": [7, 8, 9, 10],
+  "Ludwin Cuca": [7, 8, 9, 10, 11, 12],
   "Samuel Alvarez": [7, 8, 9, 10],
   "William Israel": [7, 8],
   "Yuliet Riz": [7, 8],
@@ -52,6 +52,9 @@ const SEED_STUDENTS = {
 
   // 17 de julio — lecciones 9 y 10
   "Fernando Tacam": [9, 10],
-  "Edwin Otoniel": [9, 10], // solo un nombre legible en el cuaderno
-  "Lilibeth Marroquin": [9, 10],
+  "Edwin Otoniel": [9, 10, 11, 12], // solo un nombre legible en el cuaderno
+  "Lilibeth Marroquin": [9, 10, 11, 12],
+
+  // 24 de julio — lecciones 11 y 12
+  "Carlos Pelico": [11, 12],
 };
